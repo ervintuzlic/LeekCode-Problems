ae2939aa38d76c0cb4757ede6a6048eea17f6478
Ervin Tuzlic <ervin@fragnet.net>
2022-08-16 22:58:40 +0200

    AlrgorithmExercise #2

--- LeetCode Problems/AlgorithmExercises.cs
+++ LeetCode Problems/AlgorithmExercises.cs
@@ -9,16 +9,38 @@ namespace LeetCode_Problems
 
     // 1.
     // Write a C# Sharp program to compute the sum of the two given integer values. If the two values are the same, then return triple their sum.
-    public class AlgorithmExercises
+    //public class AlgorithmExercises
+    //{
+    //    static void Main()
+    //    {
+    //        Console.WriteLine("Input two numbers:");
+    //        int x = Convert.ToInt32(Console.ReadLine());
+    //        int y = Convert.ToInt32(Console.ReadLine());
+
+    //        Console.WriteLine((x == y) ? (x + y) * 3 : "NOT SAME");
+    //    }
+    //}
+
+
+
+    // 2.
+    // Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string. 
+    // If the given string length is less than 2 return the original string.
+
+    public class Problem
     {
-        static void Main()
-        {
-            Console.WriteLine("Input two numbers:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            int y = Convert.ToInt32(Console.ReadLine());
+        static void Main() { 
+            Console.WriteLine("Input string: ");
+            
+            string myStr = Console.ReadLine();
 
-            Console.WriteLine((x == y) ? (x + y) * 3 : "NOT SAME");
-        }
+            if (myStr.Length < 2)
+                Console.WriteLine(myStr);
+            else
+            {
+                string newStr = myStr.Substring(0, 2);
+                Console.WriteLine(newStr + newStr + newStr + newStr);
+            }
+        } 
     }
-
 }
